653542779ffe0bfbab68b9a1daaf0695132f854c
KermRayan. <kermd237@gmail.com>
2024-09-15 23:23:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ global.location = "Cameroon, Douala, Yaoundé" //ur location
 
 //new
 global.botname = 'Kerm Bug v2' //ur bot name
-global.ownernumber = ['237656520674'] //ur owner number, dont add more than one
+global.ownernumber = ['237683236218'] //ur owner number, dont add more than one
 global.ownername = '🦄KgTech' //ur owner name
 global.websitex = "https://youtu.be/tLq8_qOOGFI"
 global.wagc = "https://whatsapp.com/channel/0029Vafn6hc7DAX3fzsKtn45"
@@ -29,7 +29,7 @@ global.packname = "Sticker By"
 global.author = "⚡️KgTech\n\n+237656520674"
 global.creator = "237656520674@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["237656520674"] // Premium User
+global.premium = ["237683236218"] // Premium User
 
 //channel id
 global.xchannel = {
